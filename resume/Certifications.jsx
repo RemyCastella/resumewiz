3e547cba947a3413e0cd8e56e3c2be4148c8daf6
RemyCastella <remy.jpen@gmail.com>
2024-08-19 19:19:40 +0900

    fix commit mistake

--- resume/Certifications.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Certifications() {
-  return <h1>Certifications goes here</h1>;
-}
--- resume/Experiences.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Experiences() {
-  return <h1>Experiences goes here</h1>;
-}
